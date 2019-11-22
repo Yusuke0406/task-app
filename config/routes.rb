c905ce9348438c406a9720e664bbd1950266ced5
Yusuke Harada <lefty0406_0513@yahoo.co.jp>
2019-11-22 12:39:49 +0900

    add_login_view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
+ 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "task#index"
+
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+    post 'login', to: 'devise/sessions#create'
+    delete 'signout', to: 'devise/sessions#destroy'
+  end
 end
