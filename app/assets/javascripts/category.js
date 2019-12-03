82a2c66bfd567e477f002a6b6e480e9844631835
Yusuke Harada <lefty0406_0513@yahoo.co.jp>
2019-12-03 19:28:34 +0900

    last

--- app/assets/javascripts/category.js
+++ app/assets/javascripts/category.js
@@ -1,38 +1,11 @@
-$(function(){
-  $("#fadetoggle_button").on("click", function() {
-      $("#fadetoggle_menu").slideToggle(); 
-      $("#fadetoggle_menu").toggleClass("active"); 
-  });
-});
+
 // $('#fadetoggle_menu').on('submit', function(e){
 //   e.preventDefault();
 //   console.log("ok");
 //   // var formdate = new FormData(this);
 //   // var utl
 // })
-$(function(){
-  $("input[type=checkbox]").click(function(){
-    // var tag = $('input[type=checkbox]').prop('checked');
-    var deleteConfirm = confirm('削除してよろしいでしょうか？');
 
-    if(deleteConfirm == true){
-      var task = $(".input[type=checkbox]").val();
-      console.log(task)
-      $(this).closest('.work').remove();
-      // $.ajax({
-      //   url: '/user/' + userID,
-      //   type: 'POST',
-      //   data: {'id': userID,
-      //          '_method': 'DELETE'} // DELETE リクエストだよ！と教えてあげる。
-      // })
-     
-    }else {
-      (function(e) {
-        e.preventDefault()
-      });
-    }
-  })
-})
 $(function(){
   function buildhtml(task){
     var deadline = task.deadline? `${task.deadline}` :"";
