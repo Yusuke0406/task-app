0b4a235dcc6f74e76ce926a9c5467682cee70cb3
Yusuke Harada <lefty0406_0513@yahoo.co.jp>
2020-01-26 18:21:58 +0900

    軽微な変更

--- app/assets/javascripts/add_category.js
+++ app/assets/javascripts/add_category.js
@@ -6,20 +6,11 @@ $(function(){
 });
 $(function(){
   $("input[type=checkbox]").click(function(){
-    // var tag = $('input[type=checkbox]').prop('checked');
     var deleteConfirm = confirm('削除してよろしいでしょうか？');
-
     if(deleteConfirm == true){
       var task = $(".input[type=checkbox]").val();
       console.log(task)
-      $(this).closest('.work').remove();
-      // $.ajax({
-      //   url: '/user/' + userID,
-      //   type: 'POST',
-      //   data: {'id': userID,
-      //          '_method': 'DELETE'} // DELETE リクエストだよ！と教えてあげる。
-      // })
-     
+      $(this).closest('.work').remove();     
     }else {
       (function(e) {
         e.preventDefault()
